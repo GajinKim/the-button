c512e5bcbad2022fd903d24f9ab2200d99e81e46
GaJin Kim <gajin2000@gmail.com>
2022-09-30 15:39:56 -0500

    test

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -51,7 +51,7 @@ connection = pymysql.connect(
 )
 
 def create_button_counter_table(event, context):
-    cursor = connection.cursor()
-    cursor.execute(f"CREATE TABLE `the_button`.`button_counter` (`id` INT NOT NULL, `counter` INT NOT NULL, `date` DATETIME NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE, UNIQUE INDEX `counter_UNIQUE` (`counter` ASC) VISIBLE);")
-    connection.commit()
-    return {"statusCode": 201, "body": "Successfuly created button_counter table!"}
+    # cursor = connection.cursor()
+    # cursor.execute(f"CREATE TABLE `the_button`.`button_counter` (`id` INT NOT NULL, `counter` INT NOT NULL, `date` DATETIME NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE, UNIQUE INDEX `counter_UNIQUE` (`counter` ASC) VISIBLE);")
+    # connection.commit()
+    return {"statusCode": 201, "body": "Successfuly created button_counter table!", "username": username}
