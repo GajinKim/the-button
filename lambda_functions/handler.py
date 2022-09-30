a1b83607a4506d0d242964ed3bdc0756d53d9812
GaJin Kim <gajin2000@gmail.com>
2022-09-30 18:21:46 -0500

    idk

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -2,16 +2,19 @@ from package import pymysql
 import json
 import boto3
 import base64
-import logging
 from botocore.exceptions import ClientError
 
+# connection configurations
+endpoint = "thebutton-dbprimaryinstance-86e1a6iuvxho.ck4gxkbnmkf4.us-east-1.rds.amazonaws.com"
+username = 'notapokisimp'
+password = None
+database_name = "the_button"
+connection = None
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def get_secret():
+def openConnection():
     secret_name = "arn:aws:secretsmanager:us-east-1:007633048842:secret:TheButtonSecrets-HgAPc3"
     region_name = "us-east-1"
+
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
@@ -19,8 +22,6 @@ def get_secret():
         service_name='secretsmanager',
         region_name=region_name
     )
-
-    logger.info("hellotest123")
 
     try:
         get_secret_value_response = client.get_secret_value(
@@ -39,26 +40,44 @@ def get_secret():
             raise e
     else:
         if 'SecretString' in get_secret_value_response:
-            logger.info("Inside string response...")
             secret = get_secret_value_response['SecretString']
+            j = json.loads(secret)
+            password = j['password']
         else:
-            logger.info("Inside binary response...")
-            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            print("password binary:" + decoded_binary_secret)
+            password = decoded_binary_secret.password
 
-    return json.loads(secret)  # returns the secret as dictionary
+    try:
+        if (connection is None):
+            connection = pymysql.connect(
+                host=endpoint, user=username, password=password, db=database_name, connect_timeout=5
+            )
+        elif (not connection.open):
+            connection = pymysql.connect(
+                host=endpoint, user=username, password=password, db=database_name, connect_timeout=5
+            )
+    except Exception as e:
+        raise e
+
+    # connection = pymysql.connect(
+    #     host=endpoint, user=username, password=password, db=database_name
+    # )
+
+    # return json.loads(secret)  # returns the secret as dictionary
 
 # Configuration endpoints
-secret = get_secret()
+# secret = get_secret()
 
-endpoint = "thebutton-dbprimaryinstance-86e1a6iuvxho.ck4gxkbnmkf4.us-east-1.rds.amazonaws.com"
-username = 'notapokisimp'
-password = secret["password"]
-database_name = "the_button"
+# endpoint = "thebutton-dbprimaryinstance-86e1a6iuvxho.ck4gxkbnmkf4.us-east-1.rds.amazonaws.com"
+# username = 'notapokisimp'
+# password = secret["password"]
+# database_name = "the_button"
 
-# Connection
-connection = pymysql.connect(
-    host=endpoint, user=username, password=password, db=database_name
-)
+# # Connection
+# connection = pymysql.connect(
+#     host=endpoint, user=username, password=password, db=database_name
+# )
 
 def create_button_counter_table(event, context):
     cursor = connection.cursor()
