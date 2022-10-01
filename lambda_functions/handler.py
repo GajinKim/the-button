4e42723ff273642c0060e852061dc2606c28778e
GaJin Kim <gajin2000@gmail.com>
2022-10-01 15:07:08 -0500

    formatting

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -89,7 +89,7 @@ def create_button_counter_table(event, context):
         if (connection is not None and connection.open):
             connection.close()
     # response = { "statusCode": 201, "body": "Successfuly created button_counter table!" }
-    response = { "statusCode": 201, "body": {password} }
+    response = { "statusCode": 201, "body": str(password) }
     return response
 
 def delete_button_counter_table(event, context):
