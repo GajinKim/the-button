9836635bd210484fb395202b3fe14bc01ca07b07
GaJin Kim <gajin2000@gmail.com>
2022-10-01 14:51:17 -0500

    test

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -77,16 +77,16 @@ def open_connection():
 #     print(data)
 
 def create_button_counter_table(event, context):
-    try:
-        open_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"CREATE TABLE `the_button`.`button_counter` (`id` INT NOT NULL AUTO_INCREMENT, `buton_color` VARCHAR(255) NULL, `ip_address` VARCHAR(255) NULL, `country` VARCHAR(255) NULL, `date` DATETIME NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);")
-        connection.commit()
-    except Exception as e:
-        response = { "statusCode": 400, "body": "Failed to create button_counter table!", "message": e }
-    finally:
-        if (connection is not None and connection.open):
-            connection.close()
+    # try:
+    open_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"CREATE TABLE `the_button`.`button_counter` (`id` INT NOT NULL AUTO_INCREMENT, `buton_color` VARCHAR(255) NULL, `ip_address` VARCHAR(255) NULL, `country` VARCHAR(255) NULL, `date` DATETIME NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);")
+    connection.commit()
+    # except Exception as e:
+    #     response = { "statusCode": 400, "body": "Failed to create button_counter table!", "message": e }
+    # finally:
+    #     if (connection is not None and connection.open):
+    #         connection.close()
     response = { "statusCode": 201, "body": "Successfuly created button_counter table!" }
     return response
 
