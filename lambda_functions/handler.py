298c8d20c9c0274c47772a40ff2accd711b810b8
GaJin Kim <gajin2000@gmail.com>
2022-10-01 16:59:20 -0500

    more testing

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -86,33 +86,30 @@ def create_button_counter_table(event, context):
         if (connection is not None and connection.open):
             connection.close()
     
-    response = { "statusCode": 201, "body": "Successfuly created button_counter table!" }
-    return response
+    return { "statusCode": 201, "body": "Successfuly created button_counter table!" }
 
 def delete_button_counter_table(event, context):
     try:
         open_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"DROP TABLE `the_button`.`button_counter`;")
-        connection.commit()
-    except Exception as e:
-        print(e)
+        with connection.cursor() as cursor:
+            cursor.execute(f"DROP TABLE `the_button`.`button_counter`;")
+            connection.commit()
+    except Exception:
+        return { "statusCode": 400, "body": "Failed to delete buton_counter_table"}
     finally:
         if (connection is not None and connection.open):
             connection.close()
-    response = { "statusCode": 202, "body": "Successfuly deleted button_counter table!" }
-    return response
+    return { "statusCode": 202, "body": "Successfuly deleted button_counter table!" }
 
 def insert_click_button_counter_table(event,context):
     try:
         open_connection()
-        cursor = connection.cursor()
-        cursor.execute(f"INSERT INTO `the_button`.`button_counter` (`buton_color`, `ip_address`, `country`, `date`) VALUES ('GREEN', '8.8.8.8', 'TEST', '9999-12-31 23:59:59');")
-        connection.commit()
-    except Exception as e:
-        print(e)
+        with connection.cursor() as cursor:
+            cursor.execute(f"INSERT INTO `the_button`.`button_counter` (`buton_color`, `ip_address`, `country`, `date`) VALUES ('GREEN', '8.8.8.8', 'TEST', '9999-12-31 23:59:59');")
+            connection.commit()
+    except Exception:
+        return { "statusCode": 400, "body": "Failed to insert row buton_counter_table"}
     finally:
         if (connection is not None and connection.open):
             connection.close()
-    response = { "statusCode": 203, "body": "Successfuly inserted row into button_counter table!" }
-    return response
+    return { "statusCode": 203, "body": "Successfuly inserted row into button_counter table!" }
