9cfcd84a518792fb610281c1235f2c3af5a109d9
GaJin Kim <gajin2000@gmail.com>
2022-10-01 22:39:28 -0500

    test

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function triggerClick(color) {
+async function triggerClick(color) {
   // update database
-  fetch(
+  await fetch(
     `https://ce5n80ky5e.execute-api.us-east-1.amazonaws.com/Prod/simulate_click/${color}`,
     { method: "POST" }
   );
 
   // update html
-  fetch(
-    `https://ce5n80ky5e.execute-api.us-east-1.amazonaws.com/Prod/click_counter`,
-    { method: "GET" }
+  await fetch(
+    `https://ce5n80ky5e.execute-api.us-east-1.amazonaws.com/Prod/click_counter`
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
