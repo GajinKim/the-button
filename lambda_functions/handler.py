7a465dfcc6667c4b9d927309c6fbd7cbf1ab1acf
GaJin Kim <gajin2000@gmail.com>
2022-10-01 22:03:44 -0500

    asdf

--- lambda_functions/handler.py
+++ lambda_functions/handler.py
@@ -157,10 +157,7 @@ def simulate_click(event,context):
             
             # Second update click_counter table
             cursor.execute(f"SELECT counter FROM the_button.click_counter WHERE color = '{color}'")
-            # result = int(filter(str.isdigit, str(cursor.fetchone())))
-            result = re.findall('\d+', str(cursor.fetchone()))[0]
-            # incremented_value = int(filter(str.isdigit, str(cursor.fetchone()))) + 1
-            return { "statusCode": 111, "body": result }
+            incremented_value = int(re.findall('\d+', str(cursor.fetchone()))[0]) + 1 # sanitized counter + 1
             cursor.execute(f"UPDATE `the_button`.`click_counter` SET `counter` = '{incremented_value}', `last_updated` = '{datetime_now}' WHERE (`color` = '{color}');")
             # cursor.execute(f"UPDATE `the_button`.`click_counter` SET `counter` = '42', `last_updated` = '{datetime_now}' WHERE (`color` = '{color}');")
             connection.commit()
